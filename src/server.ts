6234ebee45a1e6dd9892052f799a79fbe92eb5da
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-04-20 09:57:51 -0400

    Criando projeto e configurando dependecias de servidor e desenvolvimento

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+app.get("/", (request, response) => response.json({ status: 200, message: "hell yas" }));
+app.post("/", (resquest, response) => response.json({ status: 200, message: "You make a post" }));
+app.listen(3333, () => console.log("Subiu"));
