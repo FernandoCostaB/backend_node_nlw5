c9eed40bf8309b6f20796a7e28711aa730e1de5c
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-04-20 22:27:59 -0400

    Rotas de configuraçao

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "./repositories/SettingRepository";
+
+const routes = Router();
+routes.post("/settings", async (request, response) => {
+  const { chat, username } = request.body;
+  const settingsRepository = getCustomRepository(SettingsRepository);
+  const settings = settingsRepository.create({
+    chat,
+    username
+  })
+
+  await settingsRepository.save(settings);
+
+  return response.json(settings);
+})
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
-app.get("/", (request, response) => response.json({ status: 200, message: "hell yas" }));
-app.post("/", (resquest, response) => response.json({ status: 200, message: "You make a post" }));
-app.listen(3333, () => console.log("Subiu"));
+app.use(express.json());
+app.use(routes);
+app.listen(3333, () => console.log("Subiu, ta ok misera"));
