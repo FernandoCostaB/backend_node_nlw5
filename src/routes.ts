550bfe132d8cb94f8a25090d48fa5d2c1449b7f6
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-04-21 19:53:16 -0400

    Rotas e adicionando controller a aplicaçao!

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingRepository";
+import { SettingsController } from "./controllers/SettingsController";
+
 
 const routes = Router();
-routes.post("/settings", async (request, response) => {
-  const { chat, username } = request.body;
-  const settingsRepository = getCustomRepository(SettingsRepository);
-  const settings = settingsRepository.create({
-    chat,
-    username
-  })
 
-  await settingsRepository.save(settings);
+const settingsController = new SettingsController();
+routes.post("/settings", settingsController.create);
 
-  return response.json(settings);
-})
 export { routes };
