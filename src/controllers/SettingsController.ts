c724fd287c4cd4899e2585c246919786a4479f33
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-04-21 20:14:30 -0400

    Trativa caso o usuario ja exista

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -6,8 +6,13 @@ class SettingsController {
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
     const settingsServices = new SettingsService();
-    const settings = await settingsServices.create({ chat, username });
-    return response.json(settings);
+    try {
+      const settings = await settingsServices.create({ chat, username });
+      return response.json(settings);
+    } catch (error) {
+      return response.status(400).json({ message: error.message });
+    }
+
   }
 }
 
