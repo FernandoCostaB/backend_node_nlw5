0cc8da5c9662c5338a43218de60c55c3f5536c38
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2021-04-21 21:21:09 -0400

    rota para criar usuario

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { UsersService } from "../services/UsersService";
 
-class UserController {
+class UsersController {
   async create(request: Request, response: Response) {
     const { email } = request.body;
     const usersService = new UsersService();
@@ -14,4 +14,4 @@ class UserController {
   }
 }
 
-export { UserController }
+export { UsersController }
